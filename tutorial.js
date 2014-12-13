1512a5e944af6411e63b7ed3feade55955149347
plauer <plauer411@gmail.com>
2014-12-12 23:40:00 -0700

    creates index page. sets up jsx files with basic outline for a comment box, a comment list, and a comment form

---
+++ tutorial.js
@@ -0,0 +1,34 @@
+var CommentBox = React.creatClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+});
+
+React.render(
+  <CommentBox />,
+  document.getElementById('content')
+]);
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello, world! I am a CommentList.
+      </div>
+    );
+  }
+})
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+})
